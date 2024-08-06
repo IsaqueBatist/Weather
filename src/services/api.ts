fab8f733a2300fbfa8d9d5d4de151aa1661fe7bf
Isaque <isaquebatista400@gmail.com>
2024-08-06 17:08:13 -0300

    sim

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const APIKEY: string = '78ab983725a24d118ad202504240508'
+const APIKEY = process.env.REACT_APP_API_KEY;
+
 export const api = axios.create({
   baseURL: `http://api.weatherapi.com/v1`,
   params: {
